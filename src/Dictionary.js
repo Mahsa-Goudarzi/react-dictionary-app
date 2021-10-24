1fd040e0ac7963db50dfa84c49a2e33dafd4a388
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2021-10-24 11:17:14 +0330

    installed axios, integrated DictionaryAPI, tested it with different words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [word, setWord] = useState("");
 
-  function handleSubmit(event) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  function search(event) {
     event.preventDefault(event);
-    alert(`Search for ${word} definition`);
+
+    //api documentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleChange(event) {
     event.preventDefault(event);
-    setWord(event.target.value);
+    setWord(event.target.value.trim());
   }
 
   return (
     <div className="Dictionary">
-      <form class="row g-3" onSubmit={handleSubmit}>
+      <form class="row g-3" onSubmit={search}>
         <div class="col-auto">
           <input
             type="search"
